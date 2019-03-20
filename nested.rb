48050a949c82aa86af6889c7e7148955455eec38
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 08:41:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] =
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
     return programmer_hash
 end
