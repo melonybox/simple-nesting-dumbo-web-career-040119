b7df23728918edafe75f09c3c5176b2a943c2180
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 08:41:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+     return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:dennis_ritchie][:languages].join()
 end
 
 def adding_matz
@@ -79,8 +84,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => 'Ruby', :languages => ["LISP", "C"]
+}
+    return programmer_hash
 end
 
 def changing_alan
@@ -100,9 +107,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
-     
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +128,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
+    return programmer_hash
 end
